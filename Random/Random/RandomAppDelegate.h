296651ef523ce1c6ef9cc100d290b9202bf794ab
Thiago Brandão <thiagobrandam@gmail.com>
2014-05-28 00:17:04 -0300

    Random

---
+++ Random/Random/RandomAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RandomAppDelegate.h
+//  Random
+//
+//  Created by thiago on 5/28/14.
+//  Copyright (c) 2014 bignerdranch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RandomAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Random/Random/RandomAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  RandomAppDelegate.m
+//  Random
+//
+//  Created by thiago on 5/28/14.
+//  Copyright (c) 2014 bignerdranch. All rights reserved.
+//
+
+#import "RandomAppDelegate.h"
+
+@implementation RandomAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
