b16ba2d347b33fd7bedd6f96b6019b227b809428
Thiago Brandão <thiagobrandam@gmail.com>
2014-05-28 22:03:11 -0300

    SpeakLine

--- SpeakLine/SpeakLine/SpeakLineAppDelegate.h
+++ SpeakLine/SpeakLine/SpeakLineAppDelegate.h
@@ -8,13 +8,17 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface SpeakLineAppDelegate : NSObject <NSApplicationDelegate>
+@interface SpeakLineAppDelegate : NSObject <NSApplicationDelegate,NSSpeechSynthesizerDelegate>
 {
     NSSpeechSynthesizer *_speechSynthesizer;
+    NSArray *_voices;
 }
 
 @property (assign) IBOutlet NSWindow *window;
 @property (weak) IBOutlet NSTextField *textField;
+@property (weak) IBOutlet NSButton *stopButton;
+@property (weak) IBOutlet NSButton *speakButton;
+@property (weak) IBOutlet NSTableView *tableView;
 
 - (IBAction)stopIt:(id)sender;
 - (IBAction)sayIt:(id)sender;
--- SpeakLine/SpeakLine/SpeakLineAppDelegate.m
+++ SpeakLine/SpeakLine/SpeakLineAppDelegate.m
@@ -15,6 +15,8 @@
     // Insert code here to initialize your application
 }
 
+#pragma mark - Initializer
+
 - (id)init
 {
     self = [super init];
@@ -23,11 +25,47 @@
         NSLog(@"init");
         
         _speechSynthesizer = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+        [_speechSynthesizer setDelegate:self];
+        
+        _voices = [NSSpeechSynthesizer availableVoices];
     }
     
     return self;
 }
 
+
+#pragma mark - Data Source Informal Protocol
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tv
+{
+    return (NSInteger)[_voices count];
+}
+
+- (id)tableView:(NSTableView *)tv
+objectValueForTableColumn:(NSTableColumn *)tableColumn
+            row:(NSInteger)row
+{
+    NSString *v = [_voices objectAtIndex:row];
+    NSDictionary *dict = [NSSpeechSynthesizer attributesForVoice:v];
+    return dict[NSVoiceName];
+}
+
+#pragma mark - Delegation methods from TableView
+
+- (void)tableViewSelectionDidChange:(NSNotification *)notification
+{
+    NSInteger row = [_tableView selectedRow];
+    
+    if (row == -1) {
+        return;
+    }
+    
+    NSString *selectedVoice = _voices[row];
+    
+    [_speechSynthesizer setVoice:selectedVoice];
+    
+    NSLog(@"new voice = %@", selectedVoice);
+}
 
 - (IBAction)stopIt:(id)sender
 {
@@ -47,5 +85,35 @@
     [_speechSynthesizer startSpeakingString:string];
     
     NSLog(@"Have started to say: %@", string);
+    
+    [_stopButton setEnabled:YES];
+    [_speakButton setEnabled:NO];
+    [_tableView setEnabled:NO];
+}
+
+#pragma mark - Speech Synthesizer delegated methods
+
+- (void)speechSynthesizer:(NSSpeechSynthesizer *)sender
+        didFinishSpeaking:(BOOL)finishedSpeaking
+{
+    NSLog(@"finishedSpeaking = %d", finishedSpeaking);
+    
+    [_stopButton setEnabled:NO];
+    [_speakButton setEnabled:YES];
+    [_tableView setEnabled:YES];
+}
+
+#pragma mark - Awake from NIB
+
+- (void)awakeFromNib
+{
+    NSString *defaultVoice = [NSSpeechSynthesizer defaultVoice];
+    NSInteger defaultRow = [_voices indexOfObject:defaultVoice];
+    NSIndexSet *indices = [NSIndexSet indexSetWithIndex:defaultRow];
+    
+    [_tableView selectRowIndexes:indices
+          byExtendingSelection:NO];
+    
+    [_tableView scrollRowToVisible:defaultRow];
 }
 @end
