e598a73425f93d9f0d0b9d92f75b352baf123bc8
Thiago Brandão <thiagobrandam@gmail.com>
2014-05-29 10:57:08 -0300

    Adds line to explain protocol conformance

--- SpeakLine/SpeakLine/SpeakLineAppDelegate.h
+++ SpeakLine/SpeakLine/SpeakLineAppDelegate.h
@@ -8,6 +8,9 @@
 
 #import <Cocoa/Cocoa.h>
 
+// From the big nerd ranch forums:
+//3) In the case of this challenge, advertising that TodoController conforms to the protocol is unnecessary, but helpful. Unnecessary because Interface Builder doesn't presently check outlet protocol conformance when making connections, helpful because if you use it, you can autocomplete the protocol methods in the implementation. For example, in a class that advertises its conformance to NSTableViewDataSource, try typing "- t" and the autocomplete will include matching protocol methods in the list. A convenient (and safe) alternative to copying the method declaration by hand.
+// http://forums.bignerdranch.com/viewtopic.php?f=178&t=3221&start=10
 @interface SpeakLineAppDelegate : NSObject <NSApplicationDelegate,NSSpeechSynthesizerDelegate>
 {
     NSSpeechSynthesizer *_speechSynthesizer;
