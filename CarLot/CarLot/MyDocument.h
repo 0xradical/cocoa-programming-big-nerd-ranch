ac6a9ce081d37426cdb624806bdd2e406c2b4c2a
Thiago Brandão <thiagobrandam@gmail.com>
2014-06-02 07:03:12 -0300

    Adds CarLot

---
+++ CarLot/CarLot/MyDocument.h
@@ -0,0 +1,13 @@
+//
+//  MyDocument.h
+//  CarLot
+//
+//  Created by thiago on 6/2/14.
+//  Copyright (c) 2014 bignerdranch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument
+
+@end
---
+++ CarLot/CarLot/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CarLot
+//
+//  Created by thiago on 6/2/14.
+//  Copyright (c) 2014 bignerdranch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
